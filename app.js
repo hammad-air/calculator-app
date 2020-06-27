8c3251157c39e3dd3185f71f250d282f19c5bf9a
Hammad sheikh <hammadn788@gmail.com>
2020-06-27 08:58:25 -0700

    app calculator

---
+++ app.js
@@ -0,0 +1,25 @@
+function getNumber(num){
+    
+    var result = document.getElementById("result");
+
+   result.value  += num;
+
+}
+function clearResult(){
+
+  var result = document.getElementById("result");
+ result.value = ""
+
+}
+
+function getResult(){
+
+  var result = document.getElementById("result");
+  console.log(result.value) 
+  result.value = eval(result.value)
+
+}
+
+
+                           
+    
